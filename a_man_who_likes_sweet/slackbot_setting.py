ce4baf88e4e6785d0f360e7458e995fe8ddb5229
Stephen-LiuHan <stephensugimoto@gmail.com>
2018-10-30 13:43:54 +0900

    add

---
+++ a_man_who_likes_sweet/slackbot_setting.py
@@ -0,0 +1,9 @@
+#-*- coding UTF-8 -*-
+
+#botアカののトークンを指定
+API_TOKEN = "xoxb-274561848439-392500189632-qoyjM0aDzSHH7pQEgSQt0rpW"
+
+DEFALT_REPLY = "ん？何か言ったかい？"
+
+#プラグインスクリプトを置いてあるサブディレクトリのリスト
+PLUGINS = ['plugins']
